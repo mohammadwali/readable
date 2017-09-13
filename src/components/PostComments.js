fe8f3f1f5334a46164e4f714b32c75501acff6b1
Mohammad Wali <mohdwali@live.com>
2017-09-13 13:06:01 +0530

    Separating Comment item

--- src/components/PostComments.js
+++ src/components/PostComments.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import TimeAgo from 'react-timeago';
+
 import {SubmissionError} from 'redux-form';
 import * as EmailValidator from 'email-validator';
-import moment from 'moment';
 import * as actions from '../actions/postActions';
 
 import CommentForm from './CommentForm'
+import CommentItem from './CommentItem'
 
 
 class PostPage extends Component {
@@ -87,48 +87,6 @@ class PostPage extends Component {
     }
 }
 
-const CommentItem = ({comment, deleteComment}) => {
-
-    const formattedDate = moment(comment.timestamp).format("YYYY-MM-DD hh:mm:ss");
-
-    return ( <div className="comment-item">
-        <div className="comment-header clearfix">
-            <div className="col-md-8">
-                <span className="glyphicon glyphicon-user"/>
-                &nbsp;
-                <strong>{comment.author}</strong> commented <TimeAgo date={comment.timestamp}/> ( { formattedDate } )
-            </div>
-
-            <div className="col-md-2 pull-right">
-
-                <div className="post-score">
-                    <span className="glyphicon glyphicon-arrow-up"/>
-                    <span> {comment.voteScore}</span>
-                    <span className="glyphicon glyphicon-arrow-down"/>
-                </div>
-
-            </div>
-
-            <div className="col-md-1 text-right pull-right inline-icons-wrap">
-
-                <a className="edit-icon inline-icons"><i className="fa fa-pencil"/></a>
-                <a className="delete-icon inline-icons"
-                   onClick={() => deleteComment(comment.id, comment.parentId)}>
-                    <i className="fa fa-trash"/>
-                </a>
-
-
-            </div>
-        </div>
-        <div className="comment-body clearfix">
-
-            <div className="col-md-12"> {comment.body}</div>
-
-        </div>
-
-
-    </div>);
-};
 
 
 function mapStateToProps(store, ownProps) {
